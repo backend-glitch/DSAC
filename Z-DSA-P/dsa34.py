b9f3536183d0dc78028b04871b9212c6f13c94c3
Arjun_2025 <vineetaarjun123@gmail.com>
2025-12-06 14:32:16 +0530

    Update dsa34.py

--- Z-DSA-P/dsa34.py
+++ Z-DSA-P/dsa34.py
@@ -5,13 +5,12 @@ def lengthOfLongestSubstring(s):
     for i in range(n):
         for j in range(i, n):
 
-            # check if substring s[i:j+1] has all unique characters
+        
             substring = s[i:j+1]
-            if len(substring) == len(set(substring)):   # duplicates? no → valid
+            if len(substring) == len(set(substring)):  
                 max_len = max(max_len, j - i + 1)
 
     return max_len
 
 
-# Example
 print(lengthOfLongestSubstring("pwwkew"))
