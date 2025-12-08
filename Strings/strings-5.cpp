8bace2027ab7122bbabc78775b0eb4a099115b7e
Arjun_2025 <vineetaarjun123@gmail.com>
2025-12-08 21:19:08 +0530

    strings-5

---
+++ Strings/strings-5.cpp
@@ -0,0 +1,40 @@
+/*
+input : "the sky blue"
+output : "blue sky the"
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+      string reverseWords(string s) {
+         int n = s.length();
+         string ans = "";
+
+  reverse (s.begin(),s.end());
+  
+  for(int i=0;i<n;i++){
+    string word = "";// here we use " "
+    while(i<n && s[i] != ' '){// here we use ' '
+      word += s[i];// for every word.
+      i++;
+    }
+
+    reverse( word.begin(),word.end());//if senternce contain more than 1 spaces
+      if(word.length() > 0){
+        ans += " " + word;
+      
+    }
+  }
+  return ans.substr(1);
+  
+    }
+
+ int main(){
+
+    string word = "the sky blue";
+
+    string ans = reverseWords(word);
+    cout<<"The reverse sentence : "<<ans;
+
+
+ }
